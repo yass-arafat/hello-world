8e174a628bc9f40671b195bba3a08d1c74400a43
yass-arafat <roni.yassir@gmail.com>
2024-11-27 18:41:42 +0600

    Add working unit and integration test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -3,6 +3,7 @@ from app import hello_world
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
+        # self.assertEqual(hello_world(), "World!")
         self.assertEqual(hello_world(), "Hello, World!")
 
 if __name__ == "__main__":
