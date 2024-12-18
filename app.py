13a9ca8619809aa9af7026afe96939c49fac4af2
yass-arafat <roni.yassir@gmail.com>
2024-12-18 10:59:11 +0600

    Add README.md

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("Hello, World! Jenkins")
+    print("Hello, World! Jenkins, Test")
     return "Hello, World! Jenkins"
 
 if __name__ == '__main__':
